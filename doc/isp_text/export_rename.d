7c9e50624c03fd98cf2c552cd8dfddf1ad805cad
Daniel 'Danol' Čejchan <czdanol@gmail.com>
2016-12-02 23:54:23 +0100

    - More work on ISP, getting Code hatching concept together

--- doc/isp_text/export_rename.d
+++ doc/isp_text/export_rename.d
@@ -1,9 +1,10 @@
 import std.file;
 import std.datetime;
 import std.conv;
+import std.string;
 
 void main() {
 	SysTime tm = Clock.currTime;
 
-	copy( "projekt.pdf", tm.year.to!string ~ "-" ~ tm.month.to!ubyte.to!string ~ "-" ~ tm.day.to!string ~ "_xcejch00_isp.pdf" );
+	copy( "projekt.pdf", tm.year.to!string ~ "-" ~ tm.month.to!ubyte.to!string.rightJustify( 2, '0' ) ~ "-" ~ tm.day.to!string.rightJustify( 2, '0' ) ~ "_xcejch00_isp.pdf" );
 }
